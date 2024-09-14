2ec1382434237f6e2dfe350eca4a0a0e7d88e5ad
CADNurd <147918819+LiamG2@users.noreply.github.com>
2024-09-14 18:11:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import gspread
 
 # link to test GSheet >> https://docs.google.com/spreadsheets/d/1WX-4NFs_ITvGuDNWMkODbS2_ZISG6SFjhx-Mg4dezr0/edit?usp=sharing
 
+# Following instructions from >> https://docs.gspread.org/en/v6.1.2/oauth2.html#enable-api-access
+    # Relevant project set-up on Google Developers Console >> https://console.cloud.google.com/apis/credentials?project=opportune-geode-435020-g8
+    # Generated API Key from above project >> AIzaSyDpIvMkGb2WdHQC5xT1MHmefJZ7c3HRmlY
+
 
 st.title("Web App Title")
 st.write(
