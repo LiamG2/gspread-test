698d49506868526450f0aa6d2c34df049663a771
CADNurd <147918819+LiamG2@users.noreply.github.com>
2024-09-14 17:52:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import gspread
 
+# link to test GSheet >> https://docs.google.com/spreadsheets/d/1WX-4NFs_ITvGuDNWMkODbS2_ZISG6SFjhx-Mg4dezr0/edit?usp=sharing
+
 
 st.title("Web App Title")
 st.write(
