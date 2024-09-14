76bccb50306fbc55b1e57b94923770f86aacf576
CADNurd <147918819+LiamG2@users.noreply.github.com>
2024-09-14 18:41:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,26 +7,13 @@ import gspread
 #   Relevant project set-up on Google Developers Console >> https://console.cloud.google.com/apis/credentials?project=opportune-geode-435020-g8
 #   Generated API Key from above project >> AIzaSyDpIvMkGb2WdHQC5xT1MHmefJZ7c3HRmlY
 
-
-
-
 gApiKey = gspread.api_key("AIzaSyDpIvMkGb2WdHQC5xT1MHmefJZ7c3HRmlY")
 
 public_sheet = gApiKey.open_by_url('https://docs.google.com/spreadsheets/d/1WX-4NFs_ITvGuDNWMkODbS2_ZISG6SFjhx-Mg4dezr0/edit?usp=sharing')
 
-# sh = gc.open_by_key("AIzaSyDpIvMkGb2WdHQC5xT1MHmefJZ7c3HRmlY")
-
 cellContents = (public_sheet.sheet1.get('B3'))
 
 
-
-
-# val = worksheet.acell('B1').value
-
 st.title("Web App Title")
-st.write(
-    "Write stuff here . . ."
-)
 
 st.write(cellContents)
-
