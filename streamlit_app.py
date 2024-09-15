c0e9a91157c680c98471839ec7b3a638a631b87e
CADNurd <147918819+LiamG2@users.noreply.github.com>
2024-09-15 12:06:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import gspread
 
-# link to test GSheet >> https://docs.google.com/spreadsheets/d/1WX-4NFs_ITvGuDNWMkODbS2_ZISG6SFjhx-Mg4dezr0/edit?usp=sharing
+# link to test GSheet >> https://docs.google.com/spreadsheets/d/1pkysi4rP3zsl20GWUp_HFg3CRg44BXdaoJDI0fnqIHA/edit?usp=sharing
 
 # Following instructions from >> https://docs.gspread.org/en/v6.1.2/oauth2.html#enable-api-access
 #   Relevant project set-up on Google Developers Console >> https://console.cloud.google.com/apis/credentials?project=opportune-geode-435020-g8
@@ -9,10 +9,12 @@ import gspread
 
 gApiKey = gspread.api_key("AIzaSyDpIvMkGb2WdHQC5xT1MHmefJZ7c3HRmlY")
 
-public_sheet = gApiKey.open_by_url('https://docs.google.com/spreadsheets/d/1WX-4NFs_ITvGuDNWMkODbS2_ZISG6SFjhx-Mg4dezr0/edit?usp=sharing')
+public_sheet = gApiKey.open_by_url('https://docs.google.com/spreadsheets/d/1pkysi4rP3zsl20GWUp_HFg3CRg44BXdaoJDI0fnqIHA/edit?usp=sharing')
 
-cellContents = str((public_sheet.sheet1.get('B3')))
+# convert gspread output to plain string
+cellContents = str((public_sheet.sheet1.get('A5')))
 
+# remove unnecessary chars from both string's ends
 cellContents = cellContents.strip("[]'")
 
 st.title("Web App Title")
