498ac9150ddb42116c588ce2bdb311326eedff3e
CADNurd <147918819+LiamG2@users.noreply.github.com>
2024-09-17 13:53:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,6 @@ cellContents = cellContents.strip("[]'")
 # SEE >> https://stackoverflow.com/questions/42143302/how-can-i-remove-a-newline-character-in-a-string-in-python
 cellContents = cellContents.replace('\\n', '')
 
-st.title("Web App Titleee")
+st.title("Web App Title")
 
 st.write(cellContents)
