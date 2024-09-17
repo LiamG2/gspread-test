e1db98e46b2ab930f2b059ed19f64cb9cc56915a
CADNurd <147918819+LiamG2@users.noreply.github.com>
2024-09-17 13:26:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import gspread
+import time
 
 with st.sidebar:
-    st.write("This code will be printed to the sidebar.")
+    with st.echo():
+        st.write("This code will be printed to the sidebar.")
 
 # set sidebar initial state + dummy placeholder content
 st.set_page_config(
